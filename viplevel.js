b4e87b83546dbfc6b4bc71bbcef7a7f544a69785
Stephen0910 <281016636@qq.com>
2025-06-15 13:33:30 +0800

    Update viplevel.js

--- viplevel.js
+++ viplevel.js
@@ -1,13 +1,9 @@
-const version = 'V1.0.0';
 let body = $response.body;
 if (body) {
   try {
-    let url = $request.url;
-    if (/120\.76\.41\.\d+/.test(url)) {
-      body = body.replace(/("vip":\s*)\d+/g, '$1 9');
-    }
+    body = body.replace(/("vip":)\s*\d+/g, '$1 9');
   } catch (e) {
-    console.log(`vip modify error: ${e}`);
+    console.log('VIP 替换错误：', e);
   }
 }
 $done({ body });
